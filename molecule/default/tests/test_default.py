9bad716311898ed3543eb1c52cf761e34ed30b41
Nikolay Antsiferov <nklya@hotmail.com>
2017-11-12 19:45:01 +0300

    Add travis.yml and seven test platforms

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+# check if service is enabled and running
+def test_srv_running_and_enabled(host):
+    srv = host.service('gotty')
+    assert srv.is_running
+    assert srv.is_enabled
 
+# check if configuration file exists
+def test_srv_config_file(host):
+    f = host.file('/etc/hosts')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+# # check if service is listening on localhost (netstat must be installed)
+# def test_srv_is_listening(host):
+#     port = host.socket('tcp://127.0.0.1:80')
+#     assert port.is_listening
