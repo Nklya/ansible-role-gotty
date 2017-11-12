2a0124ce5da8d204552009897ea1499ca0ff26f5
Nikolay Antsiferov <nklya@hotmail.com>
2017-11-12 21:12:00 +0300

    Fix tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,16 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# check if service is enabled and running
 def test_srv_running_and_enabled(host):
     srv = host.service('gotty')
     assert srv.is_running
     assert srv.is_enabled
 
-# check if configuration file exists
+
 def test_srv_config_file(host):
-    f = host.file('/etc/hosts')
+    f = host.file('/etc/gotty.cfg')
     assert f.exists
+
 
 # # check if service is listening on localhost (netstat must be installed)
 # def test_srv_is_listening(host):
